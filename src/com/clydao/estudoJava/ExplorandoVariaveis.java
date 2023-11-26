ded4138669715603250a89b5cef1b91975c18a82
Clydao <euclydes.bis@gmail.com>
2023-11-25 21:51:21 -0300

    Adicionada classe ExplorandoVariaveis

---
+++ src/com/clydao/estudoJava/ExplorandoVariaveis.java
@@ -0,0 +1,17 @@
+package com.clydao.estudoJava;
+
+public class ExplorandoVariaveis {
+    public static void main(String[] args) {
+        // Declare e inicialize variáveis de diferentes tipos
+        int numero = 10;
+        double valorDecimal = 5.99;
+        char letra = 'A';
+        boolean verdadeiro = true;
+
+        // Imprima as variáveis no console
+        System.out.println("Número inteiro: " + numero);
+        System.out.println("Valor decimal: " + valorDecimal);
+        System.out.println("Caractere: " + letra);
+        System.out.println("Booleano: " + verdadeiro);
+    }
+}
