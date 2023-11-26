b26d8391bd297618db47f4c068da75ac9bb25ef2
Clydao <euclydes.bis@gmail.com>
2023-11-26 16:18:11 -0300

    praticando uso de metodos ex calculadora

---
+++ src/com/clydao/estudoJavaBasico/MetodoJava/TesteCalculadora.java
@@ -0,0 +1,22 @@
+package com.clydao.estudoJavaBasico.MetodoJava;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        // Chamando os métodos
+        calc.imprimirBoasVindas();
+        int soma = calc.somar(10, 20);
+        int subtracao = Calculadora.subtrair(20, 10);
+        int multiplicacao = calc.multiplicar(5, 4);
+        int multiplicacaoSobrecarregada = calc.multiplicar(5, 4, 3);
+
+        // Imprimindo os resultados
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Multiplicação Sobrecarregada: " + multiplicacaoSobrecarregada);
+
+    }
+}
+
