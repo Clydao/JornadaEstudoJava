61a577f627d30314bdb4fb18254f74c25086d0cb
Clydao <euclydes.bis@gmail.com>
2023-11-26 11:07:17 -0300

    mais um pouco sobre variaveis e tipo de dados

--- src/com/clydao/estudoJavaBasico/ExplorandoVariaveis.java
+++ src/com/clydao/estudoJavaBasico/ExplorandoVariaveis.java
@@ -13,5 +13,26 @@ public class ExplorandoVariaveis {
         System.out.println("Valor decimal: " + valorDecimal);
         System.out.println("Caractere: " + letra);
         System.out.println("Booleano: " + verdadeiro);
+
+        int myNumber; // Declaração de uma variável do tipo inteiro
+        double myDouble; // Declaração de uma variável do tipo ponto flutuante
+        boolean myBoolean; // Declaração de uma variável do tipo booleano
+        char myChar; // Declaração de uma variável do tipo caractere
+        String myString; // Declaração de uma variável do tipo String, que é uma classe
+
+        myNumber = 10; // Atribuição de valor à variável do tipo inteiro
+        myDouble = 3.14; // Atribuição de valor à variável do tipo ponto flutuante
+        myBoolean = true; // Atribuição de valor à variável do tipo booleano
+        myChar = 'A'; // Atribuição de valor à variável do tipo caractere
+        myString = "Hello, World!"; // Atribuição de valor à variável do tipo String
+
+        int anotherNumber = 100;
+        double anotherDouble = 9.81;
+        boolean isJavaFun = true;
+        char anotherChar = 'B';
+        String anotherString = "Learning Java";
+
+        System.out.printf("print de uma declaração ja inicializada " + " - " + anotherString + " - ");
+
     }
 }
